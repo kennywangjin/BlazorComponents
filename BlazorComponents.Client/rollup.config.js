5b9bc42a85706c0c852c4d2a487d2c076a89936f
王锦 <kennywangjin@outlook.com>
2020-01-22 12:53:47 +0800

    update client scripts

--- BlazorComponents.Client/rollup.config.js
+++ BlazorComponents.Client/rollup.config.js
@@ -1,30 +1,30 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
-import typescript from "rollup-plugin-typescript";
-import { uglify } from 'rollup-plugin-uglify';
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
 
-const isDev = process.env.build === 'development';
+const isDev = process.env.build === "development";
 
 export default {
-    input: "src/components.ts",
-    output: {
-        dir: "../BlazorComponents/wwwroot",
-        format: "iife",
-        globals: { highcharts: "Highcharts", jquery: "$" }
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript(),
-        postcss({
-            extract: true,
-            minimize: !isDev,
-            sourceMap: false
-        })
-    ].concat(isDev ? [] : [uglify()]),
-    external: ["highcharts", "jquery"],
-    watch: {
-        include: "src/**"
-    }
+  input: "src/components.ts",
+  output: {
+    dir: "../BlazorComponents/wwwroot",
+    format: "iife",
+    globals: { highcharts: "Highcharts", jquery: "$" }
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss({
+      extract: true,
+      minimize: !isDev,
+      sourceMap: false
+    })
+  ].concat(isDev ? [] : [terser()]),
+  external: ["highcharts", "jquery"],
+  watch: {
+    include: "src/**"
+  }
 };
